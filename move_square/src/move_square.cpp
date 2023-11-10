07dd3e16f31e7c42da76abaf41cecd40a094257c
arybojad <01168912@pw.edu.pl>
2023-11-11 00:06:30 +0100

    Napisany ruch po kwadracie na bazie odczytow z Time, kwadrat nie dziala :c

--- move_square/src/move_square.cpp
+++ move_square/src/move_square.cpp
@@ -3,30 +3,35 @@
 #include "std_msgs/String.h"
 #include <geometry_msgs/Twist.h>
 #include "nav_msgs/Odometry.h"
+#include <cmath>
 
 // TODO zczytac real time z symulacji
 // TODO parametry yaml
 void odomCallback(const nav_msgs::Odometry& msg)
  {
 
-  ROS_INFO("Predkosc liniowa x: [%f]", msg.twist.twist.linear.x);
-  ROS_INFO("Predkosc liniowa y: [%f]", msg.twist.twist.linear.y);
-  ROS_INFO("Predkosc liniowa z: [%f]", msg.twist.twist.linear.z);
+  // ROS_INFO("Predkosc liniowa x: [%f]", msg.twist.twist.linear.x);
+  // ROS_INFO("Predkosc liniowa y: [%f]", msg.twist.twist.linear.y);
+  // ROS_INFO("Predkosc liniowa z: [%f]", msg.twist.twist.linear.z);
 
-  ROS_INFO("Predkosc katowa x: [%f]", msg.twist.twist.angular.x);
-  ROS_INFO("Predkosc katowa y: [%f]", msg.twist.twist.angular.y);
-  ROS_INFO("Predkosc katowa z: [%f]", msg.twist.twist.angular.z);
+  // ROS_INFO("Predkosc katowa x: [%f]", msg.twist.twist.angular.x);
+  // ROS_INFO("Predkosc katowa y: [%f]", msg.twist.twist.angular.y);
+  // ROS_INFO("Predkosc katowa z: [%f]", msg.twist.twist.angular.z);
 
-  ROS_INFO("Pose x: [%f]", msg.pose.pose.position.x);
-  ROS_INFO("Pose y: [%f]", msg.pose.pose.position.y);
-  ROS_INFO("Pose z: [%f]", msg.pose.pose.position.z);
+  // ROS_INFO("Pose x: [%f]", msg.pose.pose.position.x);
+  // ROS_INFO("Pose y: [%f]", msg.pose.pose.position.y);
+  // ROS_INFO("Pose z: [%f]", msg.pose.pose.position.z);
+
+}
 
 
-
+float myround(float const &num){
+  return roundf(num*10000)/10000;
 }
 
 int main(int argc, char **argv){
  
+
   ros::init(argc, argv, "move_square");
 
   ros::NodeHandle n;
@@ -40,22 +45,46 @@ int main(int argc, char **argv){
   int count = 0;
   int state = 0;
   float vel_x = 0.0;
-  float vel_y = 0.0; 
+  float vel_y = 0.0;
 
-  int acc_time = 0;
-  int deacc_time = 0;
-  int obrot_time = 0;
+  float state0_time;
+  float state1_time;
+  float state2_time;
+  float state4_time;
 
-  int constvel_time = 0;
+  float duration; 
+  int rotation_time = 0;
+
+  float constvel_time = 2.0;
   float final_vel = 1.0;
+
+  // odczytanie czasu symulacji (SimTime) nie uzywany do niczego
+  ros::Time time = ros::Time::now();
+  double secs = time.toSec();
 
   while (ros::ok())
   {
     geometry_msgs::Twist goal;
 
     if(state == 0){
+      // reset odmierzonych czasow
+      state0_time = 0.0;
+      state1_time = 0.0;
+      state2_time = 0.0;
+      state4_time = 0.0;
+      duration = 0.0;
+
+      state1_time = ros::Time::now().toSec();
+      ROS_INFO("state0_time [%f]", myround(state0_time));
+      state = 1;
+    }
+
+    if(state == 1){
+      // ros z jakiegos powodu zerowal state_time dlatego nalezy je tu przypisac
+      if(state1_time == 0.0) state1_time = myround(ros::Time::now().toSec());
 
       vel_x += 0.1;
+      ROS_INFO("state0_time [%f]", myround(state0_time));
 
       goal.angular.x = 0.0;
       goal.angular.y = 0.0;
@@ -65,15 +94,21 @@ int main(int argc, char **argv){
       goal.linear.y = 0.0;
       goal.linear.z = 0.0;
 
-      acc_time += 1;
-
       publish_move.publish(goal);
 
-      ROS_INFO("acc tiume: [%d]", acc_time);
+      ROS_INFO("vel_x [%f]", myround(vel_x));
+      ROS_INFO("final_vel [%f]", myround(final_vel));
+      if(myround(vel_x) == myround(final_vel)) {
+        // zmierz czas przyspieszania, chyba nieuzywane
+        // acc_time = ros::Time::now().toSec() - state0_time;
+        float state2_time = myround(ros::Time::now().toSec());
+        ROS_INFO("state2_time [%f]", state2_time);
+        state = 2; 
+      }
 
-      if(roundf(vel_x * 1000)/1000 == roundf(final_vel * 1000)/1000)  state = 1; 
-
-    }else if(state == 1){
+    }else if(state == 2){
+      // przypisanie czasu wyzerowanego state2_time do faktycznej wartosci
+      if(state2_time == 0.0) state2_time = myround(ros::Time::now().toSec());
 
       goal.angular.x = 0.0;
       goal.angular.y = 0.0;
@@ -85,10 +120,12 @@ int main(int argc, char **argv){
 
       publish_move.publish(goal);
 
-      constvel_time += 1;
+      float duration = ros::Time::now().toSec() - state2_time;
+      ROS_INFO("duration [%f]", myround(duration));
+      ROS_INFO("state2_time [%f]", state2_time);
+      if(myround(duration) == constvel_time) state = 3;
 
-      if(constvel_time == acc_time) state = 2;
-    }else if(state == 2){
+    }else if(state == 3){
       vel_x -= 0.1;
 
       goal.angular.x = 0.0;
@@ -99,15 +136,13 @@ int main(int argc, char **argv){
       goal.linear.y = 0.0;
       goal.linear.z = 0.0;
 
-      deacc_time += 1;
-
       publish_move.publish(goal);
 
-      ROS_INFO("acc tiume: [%d]", acc_time);
+      float state4_time = myround(ros::Time::now().toSec());
 
-      if(roundf(vel_x * 100)/100 == 0.0)  state = 3; 
-    }else if(state == 3){
-      // sprawdzic czas symulacji
+      if(myround(vel_x) == 0.0)  state = 4; 
+    }else if(state == 4){
+      if(state4_time == 0.0) state4_time = myround(ros::Time::now().toSec());
 
       //obliczyc ile ma sie obracac
 
@@ -115,22 +150,20 @@ int main(int argc, char **argv){
 
       goal.angular.x = 0.0;
       goal.angular.y = 0.0;
-      goal.angular.z = 2.0;
+      goal.angular.z = M_PI/4; // 45 stopni / sekunde
 
       goal.linear.x = 0.0;
       goal.linear.y = 0.0;
       goal.linear.z = 0.0;
 
-      obrot_time += 1;
 
       publish_move.publish(goal);
 
-      if(obrot_time == acc_time) state = 0;
+      rotation_time = ros::Time::now().toSec() - state4_time;
+      // obrot powinien trwac 2 sekundy
+      if(round(rotation_time) == 2.0) state = 0;
 
     }
-
-
-    ROS_INFO("State: [%d]", state);
 
     ros::spinOnce();
 
