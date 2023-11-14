5a053a9c51b8ac68bedc8e5ab656a0ee63e2724f
arybojad <01168912@pw.edu.pl>
2023-11-14 17:41:57 +0100

    chyba dzialajaca odometria

--- move_square/src/move_square.cpp
+++ move_square/src/move_square.cpp
@@ -7,14 +7,13 @@
 #include <yaml-cpp/yaml.h>
 
 
-float myround(float const &num, const int precision){
-  return roundf(num*pow(10, precision))/pow(10, precision);
-}
+// float myround(float const &num, const int precision){
+//   return roundf(num*pow(10, precision))/pow(10, precision);
+// }
 
 bool is_close(float a, float b, float epsilon) {
     return std::abs(a - b) < epsilon;
 }
-
 
 geometry_msgs::Twist& setForwardSpeed(geometry_msgs::Twist& goal, float vel_x){
       goal.angular.x = 0.0;
@@ -42,18 +41,18 @@ int main(int argc, char **argv){
   ros::Rate loop_rate(10);
 
   
-  float vel_x = 0.0;
+  float vel_x;
 
   float state0_time;
   float state1_time;
   float state2_time;
+  float state3_time;
   float state4_time;
+  float acc_duration;
+  float constvel_duration;
 
   float duration; 
-  int rotation_time = 0;
-
-  float constvel_time = 2.0;
-  float final_vel = 1.0;
+  float rotation_time;
 
   /* Read yaml parameters */
   int is_right;
@@ -76,11 +75,17 @@ int main(int argc, char **argv){
 
     /* State 0 - resets measurements from previous iteration*/
     if(state == 0){
-      state0_time = 0.0;  /* Unused */
+
       state1_time = 0.0;
       state2_time = 0.0;
+      state3_time = 0.0;
       state4_time = 0.0;
       duration = 0.0;
+      acc_duration = 0.0;
+      constvel_duration = 0.0;
+      rotation_time = 0.0;
+
+      vel_x = 0.0;
 
 
       state = 1;
@@ -88,46 +93,59 @@ int main(int argc, char **argv){
 
     /* State 1 - accelerate to yaml param velocity */
     if(state == 1){
-      /* Read state1_time that was reset to 0 by ROS - NOT USED */
+      /* Read state1_time that was reset to 0 by ROS */
       if(state1_time == 0.0) state1_time = ros::Time::now().toSec();
 
       /* Hardcoded acceleration rate */
-      vel_x += 0.05;
+      vel_x += 0.025;
 
       goal = setForwardSpeed(goal, vel_x);
 
       /* Publish set velocity */
       publish_move.publish(goal);
 
-      ROS_INFO("vel_x [%f]", vel_x);
-      ROS_INFO("final_vel [%f]", final_vel);
-
       /* Check whether final_vel was reached */
-      if(is_close(vel_x, final_vel, 0.06)) state = 2; 
+      if(is_close(vel_x, constvel, 0.025)) {
+        /* Check how long acceleration took*/
+        acc_duration = ros::Time::now().toSec() - state1_time;
+        state = 2; 
+      }
 
     /* State 2 - move at constant speed */
     }else if(state == 2){
       /* Read reset state2_time */
       if(state2_time == 0.0) state2_time = ros::Time::now().toSec();
 
-      goal = setForwardSpeed(goal, final_vel);
+      goal = setForwardSpeed(goal, constvel);
+
+      /* Calculate how long should constant velocity take place
+                     based on acceleration time*/
+      constvel_duration = (side_lenght/constvel) - acc_duration;
+
+      /* Check if yaml parameters make sense */
+      if(constvel_duration < 0.001){
+        ROS_INFO("Incorrect yaml parameters");
+        break;
+      }
 
       /* Publish constant velocity every iteration*/
       publish_move.publish(goal);
 
       /* Check how long does state2 last*/
-      float duration = ros::Time::now().toSec() - state2_time;
-      ROS_INFO("duration [%f]", duration, 2);
-      ROS_INFO("state2_time [%f]", state2_time);
+      duration = ros::Time::now().toSec() - state2_time;
+      ROS_INFO("constvelduration [%f], duration [%f]", constvel_duration, duration);
 
       /* Check whether movement with constant velocity lasted enough*/
-      if(is_close(duration, constvel_time, 0.001)) state = 3;
+      if(is_close(duration, constvel_duration, 0.1)) state = 3;
 
-    /* State 3 - deaccelerating*/
+    /* State 3 - deccelerating*/
     }else if(state == 3){
+      /* Read state3_time that was reset to 0 by ROS only to make iteration lenght
+          same as for acceleration (state1) */
+      if(state3_time == 0.0) state3_time = ros::Time::now().toSec();
 
-      /* Deacceleration rate*/
-      vel_x -= 0.05;
+      /* Decceleration rate*/
+      vel_x -= 0.027; /* higher decceleration rate to ensure quicker stop due to inertia*/
 
       /* Set decreasing speed */
       goal = setForwardSpeed(goal, vel_x);
@@ -135,17 +153,14 @@ int main(int argc, char **argv){
       publish_move.publish(goal);
 
       /* Check whether robot stopped */
-      if(is_close(vel_x, 0.0, 0.01))  state = 4; 
+      // modle sie do Boga aby on zwalnial tyle samo co przyspieszal xdd
+      if(is_close(vel_x, 0.0, 0.027))  state = 4; 
 
       /* State 4 - rotating*/
     }else if(state == 4){
 
       /* Read state4 begin time*/
       if(state4_time == 0.0) state4_time = ros::Time::now().toSec();
-
-      //obliczyc ile ma sie obracac
-
-      // na sam koniec sprawdzic czy minal wskazany czas na obrot
 
       goal.angular.x = 0.0;
       goal.angular.y = 0.0;
@@ -156,26 +171,22 @@ int main(int argc, char **argv){
         goal.angular.z = -M_PI/8;
       }
       
-
       goal.linear.x = 0.0;
       goal.linear.y = 0.0;
       goal.linear.z = 0.0;
-
 
       publish_move.publish(goal);
 
       rotation_time = ros::Time::now().toSec() - state4_time;
 
       /* Check whether robot finished rotating*/
-      if(is_close(rotation_time, 4.0, 0.0001)) state = 0;
-
+      if(is_close(rotation_time, 4.0, 0.01)) state = 0;
     }
 
     ros::spinOnce();
 
     loop_rate.sleep();
-   }
- 
- 
+    // ROS_INFO("state [%d]", state);
+   } 
    return 0;
  }
