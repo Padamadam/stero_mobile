35280826ac2537c103eb1b63a587b6337d6196c8
Twoje Imię Nazwisko <toja@example.com>
2023-11-16 00:56:51 +0100

    ogarniete dane do sprawka

--- rosbag/readbag.py
+++ rosbag/readbag.py
@@ -30,8 +30,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Odczytaj plik do ktorego spakowano pomiary rosbagiem
-b = bagreader('/home/adam/tiago_ws/src/stero_mobile/rosbag/r_s5_v1_a05.bag')
-
+# b = bagreader('/home/adam/tiago_ws/src/stero_mobile/rosbag/r_s5_v1_a05.bag')
+b = bagreader('/home/lnowaczy/tiago_ws/src/stero_mobile/rosbag/r_s5_v1_a05.bag')
 # Wyprintuj tabele topicow aby dowiedziec sie jakie sie zapisaly i ile maja recordow
 print(b.topic_table)
 
@@ -50,15 +50,19 @@ hODO = pd.read_csv(ODO_MSG)
 set_linearx_data = hSETVAL['linear.x'].tolist()
 set_linearx_time = hSETVAL['Time'].tolist()
 
-set_angularz_data = hSETVAL['angular.x'].tolist()
+set_angularz_data = hSETVAL['angular.z'].tolist()
 
 real_linearx_data = hODO['twist.twist.linear.x'].tolist()
 real_linearx_time = hODO['Time'].tolist()
+
+real_angularz_data = hODO['twist.twist.angular.z'].tolist()
 
 # Zzipuj dane z czasem ich odczytu aby znac moment odczytania rekordu
 zipped_real_linear = list(zip(real_linearx_data, real_linearx_time))
 zipped_set_linear = list(zip(set_linearx_data, set_linearx_time))
 
+zipped_real_angular = list(zip(real_angularz_data, real_linearx_time))
+zipped_set_angular = list(zip(set_angularz_data, set_linearx_time))
 
 # teraz niezle jajca, bo ogolnie to dwa topici publikuja dane z rozna czestotliwoscia
 # wiec aby wgl moc przyrownac do siebie rekordy predkosci zadanej i faktycznej
@@ -70,21 +74,42 @@ total_linearx_error = 0
 linearx_error_table = []
 state = 0
 
+#ruch jednostajny
+const_vel_error = []
+total_const_vel_error = 0
+
+#ruch opozniony
+delay_vel_error =[]
+total_delay_vel_error = 0
+
+set_delay_vel_analysis = []
+real_delay_vel_analysis = []
+
 constvel = 1.0
+
+state = 0
+
+i = 1
 
 # przeiteruj po wszystkich rekordach
 for xreal, xset in itertools.product(zipped_real_linear, zipped_set_linear):
     # jesli w przyblizeniu maja one ten sam czas odczytu
     # to przepisz je do listy tak aby dane w niej odpowiadaly sobie
     # wedlug chwili odczytu
+
     if round(xreal[1], 1) == round(xset[1], 1):
         # te rekordy byly pobrane w tej samej chwili
-
         # okresl czy jest to stan 1 powinno jeszcze sprawdzac predkosc katowa ze jest 0
         # albo czy juz jestesmy w stanie sprawdzania stanu 1
-        if (xset[0] == 0 and round(xreal[0], 2) == 0) or state == 1:
+
+        if state == 0:
+            # stan rozpoznajacy poczatek cyklu
+            if (xset[0] == 0 and round(xreal[0], 2) == 0):
+                state = 1
+
+        if  state == 1: # or (xset[0] == 0 and round(xreal[0], 2) == 0):
+            
             # stan 1 - przyspieszanie
-            state = 1
 
             linearx_error = abs(xreal[0] - xset[0])
             linearx_error_table.append(linearx_error)
@@ -93,23 +118,101 @@ for xreal, xset in itertools.product(zipped_real_linear, zipped_set_linear):
             set_linearx_analysis.append(xset[0])
             real_linearx_analysis.append(xreal[0])
 
-            if (xset[0] == constvel and round(xreal[0], 2) == constvel):
+            # sprawdz czy juz dojechal do predkosci koncowej
+            if (xset[0] == constvel and round(xreal[0], 2) == constvel): #or state == 2:
+                #jazda v_const
                 state = 2
-                break
+        if state == 2:
+        # stan 2 - ruch jednostajny prostoliniowy
+            momentary_const_vel_error = abs(xreal[0] - xset[0])
+            const_vel_error.append(momentary_const_vel_error)
+            total_const_vel_error += momentary_const_vel_error
 
+            if (xset[0] != constvel and round(xreal[0], 2) == constvel):
+                state = 3
+        if state == 3:
+        # stan 3 - ruch opozniony
+            momentary_delay_vel_error = abs(xreal[0] - xset[0])
+            delay_vel_error.append(momentary_delay_vel_error)
+            total_delay_vel_error += momentary_delay_vel_error
+        
+            set_delay_vel_analysis.append(xset[0])
+            real_delay_vel_analysis.append(xreal[0])
 
+            if (xset[0] == 0 and round(xreal[0], 2) == 0):
+                state = 4
+        if state == 4:
+            # break wywala z petli po pierwszym boku kwadratu
+            # break
+            # state 0 powtarza petle 4 razy
+            state = 0
+
+state=0
+angularz_error_table=[]
+total_angularz_error=0
+set_angularz_analysis=[]
+real_angularz_analysis=[]
+total_of_totals_error_angular = 0
+
+for zreal, zset in itertools.product(zipped_real_angular, zipped_set_angular):
+    if round(zreal[1], 1) == round(zset[1], 1):
+        if state == 0:
+        # stan rozpoznajacy poczatek cyklu obrotu
+            if (zset[0] != 0 and round(zreal[0], 2) != 0):
+                state = 1
+        if state == 1:
+            momentary_angularz_error = abs(zreal[0]-zset[0])
+            angularz_error_table.append(momentary_angularz_error)
+            total_angularz_error += momentary_angularz_error
+
+            set_angularz_analysis.append(zset[0])
+            real_angularz_analysis.append(zreal[0])
+            if (zset[0] == 0 and round(zreal[0], 2) == 0):
+                # po skonczonym obrocie dodajemy blad z calego obrotu do zmiennej
+                # bledu po 4 obrotach
+                total_of_totals_error_angular += total_angularz_error
+                state = 0
+                
 
 # idk label nie dziala :ccc
+
+# Wykresy ruchu przyspieszonego
 plt.figure(1)
 plt.plot(real_linearx_analysis, label='real')
 plt.plot(set_linearx_analysis, label = 'set')
 
 plt.figure(2)
 plt.plot(linearx_error_table)
+
+# Wykresy ruchu jednostajnego
+plt.figure(3)
+plt.plot(const_vel_error)
+
+# Wykresy ruchu opoznionego
+plt.figure(4)
+plt.plot(real_delay_vel_analysis, label='real')
+plt.plot(set_delay_vel_analysis, label = 'set')
+
+plt.figure(5)
+plt.plot(delay_vel_error)
+
+# Wykresy predkosci katowej
+plt.figure(6)
+plt.plot(real_angularz_analysis, label='real')
+plt.plot(set_angularz_analysis, label = 'set')
+
+plt.figure(7)
+plt.plot(angularz_error_table)
 plt.show()
 
+# dane potrzebne do sprawka
 
+# całkowita suma błedow (liniowych) - z jednej prostej
+# jezeli w pierwszej petli w ostatnim if dasz state = 0 zamiast brake bedzie to blad po calym kwadracie
+total_of_totals_error = total_linearx_error + total_const_vel_error + total_delay_vel_error
+print(f'suma bledow liniowych to {total_of_totals_error}')
 
-
-
+# calkowita suma bledow (katowych) - z calego kwadratu 
+total_of_totals_error_angular
+print(f'suma bledow katowych to {total_of_totals_error_angular}')
 
